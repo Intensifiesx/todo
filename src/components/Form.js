56a1da021b9837f8b47de0ae5a8f7d8f29d0f40a
Zain <hindizain12@yahoo.com>
2022-10-24 19:50:27 -0700

    Added delete and complete to Todo

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Form({ inputText, setInputText, todos, setTodos }) {
+function Form({ inputText, setInputText, todos, setTodos, setStatus }) {
   function inputTextHandler(e) {
     setInputText(e.target.value);
   }
@@ -10,16 +10,24 @@ function Form({ inputText, setInputText, todos, setTodos }) {
       ...todos,
       { text: inputText, completed: false, id: Math.random() * 1000 },
     ]);
+    setInputText("");
   }
-
+  function statusHandler(e) {
+    setStatus(e.target.value);
+  }
   return (
     <form>
-      <input onChange={inputTextHandler} type="text" className="todo-input" />
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
